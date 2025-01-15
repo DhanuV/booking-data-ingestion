9402d4b05741818a4fb7054bd42ddadccabdbb90
DhanuV <itsmedhanuv@gmail.com>
2025-01-16 02:00:14 +0530

    Controller to get booking based on filter

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -10,4 +10,31 @@ const createBooking = async (req, res) => {
   }
 };
 
-module.exports = { createBooking};
+//Get bookings
+const getBookings = async (req, res) => {
+  try {
+    const { date, vendor, page, limit } = req.query;
+    const result = await bookingService.getBookings({ date, vendor }, page, limit);
+
+    if (result.success) {
+      return res.status(200).json({
+        success: true,
+        data: result.data.bookings,
+        pagination: result.data.pagination,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        message: result.error || 'Failed to fetch bookings.',
+      });
+    }
+  } catch (err) {
+    console.error('Error in getBookings controller:', err);
+    return res.status(500).json({
+      success: false,
+      message: 'An internal server error occurred.',
+    });
+  }
+};
+
+module.exports = { createBooking, getBookings };
