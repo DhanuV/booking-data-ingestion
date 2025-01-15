3732654d3371a7b9e97fa496080d2c30ed5224b4
DhanuV <itsmedhanuv@gmail.com>
2025-01-16 02:00:48 +0530

    Route to fetch paginated bookings based on filter

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,5 +4,6 @@ const bookingController = require('../controllers/bookingController');
 const router = express.Router();
 
 router.post('/bookings', bookingController.createBooking);
+router.get('/bookings', bookingController.getBookings);
 
 module.exports = router;
